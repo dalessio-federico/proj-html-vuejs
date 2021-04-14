f4a443e1aaec4f67e8f3630b707d652094dd1d8e
federico d'alessio <federico1993.d.alessio.1993@gmail.com>
2021-04-14 14:28:01 +0200

    Aggiunta elementi vue

--- js/master.js
+++ js/master.js
@@ -4,6 +4,36 @@ var app = new Vue(
 {
     el : "#app",
 
+    data : 
+    {
+        header : 
+        {
+            logo :"img/av-freelance-logo-retina.png",
+            leftMenu: ["fas fa-plus",
+                        "fas fa-bars"
+                        ]
+        },
+
+        footer : 
+        {
+            logo: "img/avada-footer-logo.png",
+            credits : 
+                    [
+                        "© Copyright 2012-20202",
+                        "Avada Theme by",
+                        "All rights Reserved",
+                        "Powerd by"
+                    ],
+            socialMedia : 
+                        [
+                            "fab fa-facebook-square",
+                            "fab fa-twitter-square",
+                            "fab fa-instagram",
+                            "fab fa-youtube"
+                        ]
+        }
+    },
+
     mounted(){
         console.log("hello")
     }
